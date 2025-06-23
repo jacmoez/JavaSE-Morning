89c06e2b0a8ca691738c23db73a17dd0042bae49
Zay Yar Phyo <streammineking@gmail.com>
2025-06-23 09:28:20 +0630

    Project Day - 2

---
+++ JavaSE/Day 24/shop/apple/ProductServices.java
@@ -0,0 +1,15 @@
+package shop.apple;
+
+public class ProductServices {
+    public void addProduct(){
+        System.out.println("Welcome To Add Product");
+    }
+
+    public void deleteProduct(){
+        System.out.println("Welcome To Delete Product");
+    }
+
+    public void viewProduct(){
+        System.out.println("Welcome To View Product ");
+    }
+}
---
+++ JavaSE/Day 24/shop/apple/Transactions.java
@@ -0,0 +1,8 @@
+package shop.apple;
+
+public class Transactions {
+
+    public void transactionsView(){
+        System.out.println("Welcome To Transactions View");
+    }
+}
