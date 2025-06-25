2f30b752b1477bd62f55948a00e09ea7f2104eec
Zay Yar Phyo <streammineking@gmail.com>
2025-06-25 08:22:21 +0630

    Delete JavaSE/shop/apple directory

--- JavaSE/shop/apple/Main.java
+++
@@ -1,8 +0,0 @@
-package shop.apple;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Apple Shop");
-        new Login().beginLogin();
-    }
-}
--- JavaSE/shop/apple/Transactions.java
+++
@@ -1,8 +0,0 @@
-package shop.apple;
-
-public class Transactions {
-
-    public void transactionsView(){
-        System.out.println("Welcome To Transactions View");
-    }
-}
