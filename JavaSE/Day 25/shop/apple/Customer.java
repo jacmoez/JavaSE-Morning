da83f399b92eda24aaf874a4c846a3ee6cc48af1
Zay Yar Phyo <streammineking@gmail.com>
2025-06-25 08:22:52 +0630

    Add files via upload

---
+++ JavaSE/Day 25/shop/apple/Customer.java
@@ -0,0 +1,12 @@
+package shop.apple;
+
+public class Customer {
+
+    public static int customerCount = 0;
+
+    public  static Customer[] customers = new Customer[100];
+
+    public void viewCustomer(){
+        System.out.println("Welcome To Customer View");
+    }
+}
