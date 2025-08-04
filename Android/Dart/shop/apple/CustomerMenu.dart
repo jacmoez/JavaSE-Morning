b42442c224ba3638fe7afa75f84b06c659f8ea80
Zay Yar Phyo <streammineking@gmail.com>
2025-08-04 08:28:31 +0630

    Create CustomerMenu.dart

---
+++ Android/Dart/shop/apple/CustomerMenu.dart
@@ -0,0 +1,14 @@
+class CustomerMenu {
+
+  //Method to show Customer Menu
+  static void showMenu(){
+      print("\n");
+      print("----------------------------");
+      print("|      Product Manager      |");
+      print("----------------------------");
+      print("[1] View Products");
+      print("[2] Purchase Product");
+      print("[3] View My Transacctions");
+      print("[0] Logout");
+  }
+}
