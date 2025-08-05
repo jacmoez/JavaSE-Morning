acfdb6b5146cd644e1e10354311336ca03399ca8
Zay Yar Phyo <streammineking@gmail.com>
2025-08-05 08:54:10 +0630

    Update CustomerMenu.dart

--- Android/Dart/shop/apple/CustomerMenu.dart
+++ Android/Dart/shop/apple/CustomerMenu.dart
@@ -1,14 +1,66 @@
-class CustomerMenu {
+import 'dart:io';
 
+import 'Login.dart';
+import 'ProductServices.dart';
+
+class CustomerMenu {
   //Method to show Customer Menu
-  static void showMenu(){
+  static void showMenu() {
+    print("\n");
+    print("----------------------------");
+    print("|      Product Manager      |");
+    print("----------------------------");
+    print("[1] View Products");
+    print("[2] Purchase Product");
+    print("[3] View My Transacctions");
+    print("[0] Logout");
+  }
+
+  //Method for Customer Menu Options
+  static void options() {
+    bool cMenuRunning = true;
+
+    while (cMenuRunning) {
+      showMenu();
+
+      stdout.write("To get started, select a number: ");
+      String? input = stdin.readLineSync();
       print("\n");
-      print("----------------------------");
-      print("|      Product Manager      |");
-      print("----------------------------");
-      print("[1] View Products");
-      print("[2] Purchase Product");
-      print("[3] View My Transacctions");
-      print("[0] Logout");
+
+      int? number = int.tryParse(input!);
+
+      switch (number) {
+        case 1:
+          print("----------------------------");
+          print("|     Apple Inventory      |");
+          print("----------------------------");
+          Productservices.viewProducts();
+          print("\n");
+          break;
+
+        case 2:
+          print("----------------------------");
+          print("|     Purchase Product     |");
+          print("----------------------------");
+          Productservices.purchaseProduct();
+          print("\n");
+          break;
+        case 3:
+          print("----------------------------");
+          print("|      Transactions        |");
+          print("----------------------------");
+          print(" Transcation View Customer Transactions Comming Soon...");
+          print("\n");
+          break;
+        case 0:
+          print("Logging Out...");
+          cMenuRunning = false;
+          Login.beginLogin();
+          break;
+
+        default:
+          print("Invalid option. Try again.");
+      }
+    }
   }
 }
