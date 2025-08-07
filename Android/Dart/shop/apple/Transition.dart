9730607c5f6117fbe37d761b9ee55c5275c62b54
Zay Yar Phyo <streammineking@gmail.com>
2025-08-07 08:26:27 +0630

    Update Transition.dart

--- Android/Dart/shop/apple/Transition.dart
+++ Android/Dart/shop/apple/Transition.dart
@@ -37,4 +37,43 @@ class Transition {
 
     return "$year-$month-$day $hour:$minute:$second";
   }
+
+  //view all transactions (Admin)
+  static void viewAllTransactions() {
+    seedIfNeeded();
+
+    if (transcations.isEmpty) {
+      print("No sales have been made yet.");
+      return;
+    }
+
+    print("\n=== ALL SALES TRANACTIONS ===");
+    for (var i in transcations) {
+      print(
+        "Customer: ${i.customerName} | Product: ${i.productName} | Qty: ${i.quantity} | Unit Price: \$${i.price?.toStringAsFixed(2)} | Total : \$${(i.price! * i.quantity!).toStringAsFixed(2)} | Date: ${formatDate(i.transcationDate!)}",
+      );
+    }
+  }
+
+  //view transaction for a specific customer
+  static void viewCustomerTransaction(String customerName) {
+    seedIfNeeded();
+
+    var filtered = transcations
+        .where((a) => a.customerName!.toLowerCase() == customerName.toLowerCase())
+        .toList();
+
+    print("\n=== ALL FOR ${customerName} ===");
+
+    if (filtered.isEmpty) {
+      print("You have no transactions.");
+      return;
+    }
+
+    for (var i in filtered) {
+      print(
+        "Product ${i.productName} | Qty: ${i.quantity} | Unit Price: \$${i.price!.toStringAsFixed(2)} | Total: \$${(i.price! * i.quantity!).toStringAsFixed(2)} | Date: ${formatDate(i.transcationDate!)}",
+      );
+    }
+  }
 }
